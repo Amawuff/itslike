27ff9a6a9c2895f489d7e6521c51554554f4e6eb
Amawuff <jbuchalter@doner.com>
2015-06-02 15:37:51 -0400

    Getting Spotify Data Now

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,9 +1,32 @@
 'use strict';
 
 angular.module('itslikeApp')
-  .controller('MainCtrl', function ($scope, $http) {
-  	$scope.tempo = 0;
-    
- 
+    .controller('MainCtrl', function($scope, $http) {
+            $scope.tempo = 0;
+            $scope.originaltrack = "";
+            $scope.artist = "";
+            $scope.track = "";
 
-  });
+            $scope.processData = function(event) {
+                console.log("LOL CLICK!");
+                event.preventDefault();
+                var temp = $scope.originaltrack.split("-");
+                $scope.artist = temp[0];
+                $scope.track = temp[1];
+                $.ajax({
+                    url: 'https://api.spotify.com/v1/search',
+                    data: {
+                        q: 'artist:'+$scope.artist+' track:'+$scope.track,
+                        type: 'track'
+                    },
+                    success: function(response) {
+                        console.log(response);
+                    }
+                });
+
+
+
+            };
+        });
+
+ 
