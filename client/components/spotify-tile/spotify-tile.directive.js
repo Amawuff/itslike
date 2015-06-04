e9d09cf19c0d3535c83b2f25da1133801d5e7c27
Amawuff <jbuchalter@doner.com>
2015-06-04 14:29:45 -0400

    Spotify Tile styled, added preview function, needs pause functionality.

--- client/components/spotify-tile/spotify-tile.directive.js
+++ client/components/spotify-tile/spotify-tile.directive.js
@@ -1,15 +1,32 @@
 'use strict';
 
 angular.module('itslikeApp')
-  .directive('spotifyTile', function () {
-    return {
-      templateUrl: 'components/spotify-tile/spotify-tile.html',
-      restrict: 'E',
-      scope:{result:'='},
-      link: function (scope, element, attrs) {
-      	
-      }
-    };
-  });
+    .directive('spotifyTile', function() {
+        return {
+            templateUrl: 'components/spotify-tile/spotify-tile.html',
+            restrict: 'E',
+            scope: {
+                result: '='
+            },
+            link: function(scope, element, attrs) {
 
- 
+
+                scope.playPreview = function(songurl, event) {
+                    console.log("CLICKED THE ALBUM ART");
+                    console.log(event.target);
+                    console.log(songurl);
+                    if ($(event.target).hasClass('playing')) {
+                        scope.audioObject.pause();
+                        $(event.target).removeClass('playing');
+                    } else {
+                        scope.audioObject = new Audio(songurl);
+                        scope.audioObject.play();
+                        $(event.target).addClass('playing');
+                    }
+
+                }
+            }
+        };
+
+
+    });
